4fa7bb0c3e582b22e64eac01b363461b15ca3735
Michael Brantley <brantley@flox.dev>
2025-06-26 21:46:58 +0100

    fix: forgot to add make.bash

---
+++ src/make.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eu
+out=@out@
+
+# GNU make wrapper which invokes the make command twice, once to create the
+# make database for processing by makedot, and then to actually run the make
+# command.
+
+_MAKEDB="$(mktemp)"
+$out/libexec/make --just-print --print-data-base "$@" > "$_MAKEDB"
+$out/libexec/makedot --targets --variables --png \
+  --rewrite _out \
+  --rewrite TMPDIR \
+  --rewrite FLOX_ENV \
+  --rewrite BUILD_RESULT_FILE \
+  $_MAKEDB
+rm -f $_MAKEDB
+
+exec $out/libexec/make "$@"
